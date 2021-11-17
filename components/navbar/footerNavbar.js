cb033e9292b47bcb34af2e8daebca02ba42f4fd8
b0ldrobott <erin.gwc@gmail.com>
2021-11-17 22:18:19 +0900

    added bottom navbar sticky

--- components/navbar/footerNavbar.js
+++ components/navbar/footerNavbar.js
@@ -3,16 +3,24 @@ import dynamic from "next/dynamic";
 import ChakraNextLink from "../chakraNextLink";
 
 function FooterNavbar() {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode } = useColorMode();
   const DynamicDarkMode = dynamic(() => import("../darkMode"), { ssr: false });
   return (
     <Flex
       display={{ base: "flex", md: "none" }}
       justifyContent="flex-end"
-      pt={3}
+      bg={colorMode === "light" ? "white" : "gray.900"}
       px={3}
       gridGap={2}
       align="center"
+      minH="6vh"
+      maxH="6vh"
+      zIndex="10"
+      sx={{
+        position: "-webkit-sticky" /* Safari */,
+        position: "sticky",
+        bottom: "0",
+      }}
     >
       <ChakraNextLink to="/">Home</ChakraNextLink>
       <ChakraNextLink to="/about">About</ChakraNextLink>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { Heading } from "@chakra-ui/react";
+
 export default function Home() {
-  return <div>Homepage</div>;
+  return <Heading>Homepage</Heading>;
 }
